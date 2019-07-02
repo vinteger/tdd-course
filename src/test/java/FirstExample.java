847ababec346346d428ebb82ac551cde93686869
Vaughn Walker <vaughn.walker@integral.io>
2019-07-02 13:51:42 -0400

    Setup project with failing test

---
+++ src/test/java/FirstExample.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstExample {
+
+    @Test
+    public void firstTest(){
+        assertEquals(2 + 2, 3);
+    }
+}
